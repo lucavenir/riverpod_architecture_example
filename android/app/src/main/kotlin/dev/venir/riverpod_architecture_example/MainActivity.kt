846ffeadfe164f3c3e1192ce91276bb1bffe8c19
venir.dev <luca@venir.dev>
2023-04-27 19:21:29 +0200

    flutter create, dependencies, cleanup

---
+++ android/app/src/main/kotlin/dev/venir/riverpod_architecture_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.venir.riverpod_architecture_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+// ignore_for_file: file_names
+
+import 'features/init/init.dart';
+
+Future<void> main() async => init();
