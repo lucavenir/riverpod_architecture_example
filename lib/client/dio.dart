90112dbf380fd12f96d70f38c2589b9b142d1a13
venir.dev <luca@venir.dev>
2023-06-20 19:27:16 +0200

    Defined an HttpClient

---
+++ lib/client/dio.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:logging/logging.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../logs/logger.dart';
+import '../logs/logger_color.dart';
+
+part 'dio.g.dart';
+
+@riverpod
+Dio httpClient(
+  HttpClientRef ref, {
+  bool logRequestHeader = false,
+  bool logRequestBody = false,
+  bool logResponseHeader = false,
+  bool logResponseBody = true,
+}) {
+  final logger = ref.watch(loggerProvider('Dio', color: LoggerColor.cyan, level: Level.FINE));
+
+  final options = BaseOptions(
+    baseUrl: 'TODO',
+    receiveTimeout: 12.seconds,
+  );
+
+  final loggerInterceptor = PrettyDioLogger(
+    logPrint: logger.fine,
+    maxWidth: 120,
+    compact: false, // TODO play with this
+    requestHeader: logRequestHeader,
+    requestBody: logRequestBody,
+    responseHeader: logResponseHeader,
+    responseBody: logResponseBody,
+  );
+  final dio = Dio(options);
+  if (kDebugMode) dio.interceptors.add(loggerInterceptor);
+
+  ref.onDispose(dio.close);
+  return dio;
+}
