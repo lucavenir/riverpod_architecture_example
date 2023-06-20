f35ec91516cef98661d855434a8cb047637f88ea
venir.dev <luca@venir.dev>
2023-06-20 22:42:51 +0200

    Connected an actual App w/ runApp

---
+++ lib/core/app.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
+import 'package:riverpod_architecture_example/router/routes.dart';
+import 'package:riverpod_architecture_example/theme/dark_theme.dart';
+import 'package:riverpod_architecture_example/theme/light_theme.dart';
+
+class App extends HookWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final key = useRef(GlobalKey<NavigatorState>(debugLabel: 'routerKey'));
+    final router = useMemoized(
+      () => GoRouter(
+        navigatorKey: key.value,
+        debugLogDiagnostics: kDebugMode,
+        initialLocation: const HomeRoute().location,
+        routes: $appRoutes,
+      ),
+    );
+    useEffect(() => router.dispose, [router]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    // TODO disable android OS backbutton
+
+    return MaterialApp.router(
+      routerConfig: router,
+      title: 'FlGuard',
+      theme: lightTheme,
+      darkTheme: darkTheme,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,16 @@
 // ignore_for_file: file_names
 
-import 'src/init/init.dart';
+import 'package:flutter/widgets.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_architecture_example/core/app.dart';
+import 'package:riverpod_architecture_example/core/init.dart';
 
-Future<void> main() async => init();
+Future<void> main() async {
+  final (logger, crashlytics) = init();
+  runApp(
+    ProviderScope(
+      observers: [logger, crashlytics],
+      child: const App(),
+    ),
+  );
+}
