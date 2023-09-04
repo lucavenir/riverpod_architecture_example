a71aeffb18d6bca1747ed4cd3719119969c2a376
LeoDaVeenchy <arturoromanelli13@gmail.com>
2023-09-04 16:24:11 +0200

    Changed responsibility of `LocationsRepository`

--- lib/src/search/domain/services/locations_service.dart
+++ lib/src/search/domain/services/locations_service.dart
@@ -1,7 +1,6 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../data/repository/locations_repository.dart';
-import '../adapters/locations_from_dto.dart';
 import '../entities/locations.dart';
 
 part 'locations_service.g.dart';
@@ -17,10 +16,6 @@ class LocationsService {
   final LocationsRepository repository;
 
   Future<Locations> getLocations(String search) async {
-    final result = await repository.getLocations(search);
-    final places = result.map((e) => e.toDomain());
-    final locations = Locations(places: places.toList());
-
-    return locations;
+    return repository.getLocations(search);
   }
 }
