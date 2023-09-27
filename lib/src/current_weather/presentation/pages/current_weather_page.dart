0c5c7f6b0de2d26306d6b22b2f97ee8544ba35d5
Arturo Romanelli <97022239+ArturoRomanelli@users.noreply.github.com>
2023-09-27 16:37:22 +0200

    Added snackBar that shows if you're online. (#6)
    * Added new method of dealing with environment
    * Added connectivity snackbar
    * Fixed minor problem with snackbar
    * Solved merge conflicts
    
    ---------
    
    Authored-by: Arturo Romanelli <arturo.romanelli@wde.it>
    Co-authored-by: Luca Venir <luca@venir.dev>

--- lib/src/current_weather/presentation/pages/current_weather_page.dart
+++ lib/src/current_weather/presentation/pages/current_weather_page.dart
@@ -1,9 +1,11 @@
 // ignore_for_file: use_colored_box
 
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../../../client/connectivity_stream_provider.dart';
 import '../../../forecast/presentation/widgets/forecast_widget.dart';
 import '../../../locations/presentation/state/current_weather_location.dart';
 import '../../../locations/presentation/widgets/search_bar.dart';
@@ -21,6 +23,19 @@ class CurrentWeatherPage extends HookConsumerWidget {
       currentLocationControllerProvider.select((value) => value.valueOrNull?.cityName ?? ''),
     );
     final currentWeather = ref.watch(currentWeatherProvider);
+
+    ref.listen(connectivityStreamProvider, (_, next) {
+      final data = next.asData;
+      if (data == null) return;
+
+      final hasInternet = data.value != ConnectivityResult.none;
+
+      final message = hasInternet ? 'You are online' : 'You are offline';
+      final color = hasInternet ? Colors.green : Colors.red;
+
+      final snackBar = SnackBar(content: Text(message), backgroundColor: color);
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    });
 
     return Scaffold(
       appBar: AppBar(
